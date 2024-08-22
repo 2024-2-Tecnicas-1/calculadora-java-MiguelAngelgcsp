4c4f91bc18b5e64d9e6b85af09a0b81e2a78a530
samuelalejo18 <samuelalejomonsalve@gmail.com>
2024-08-22 09:38:20 -0500

    Se sube el quiz de java

--- src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
+++ src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
@@ -26,7 +26,14 @@ public class Calculadora {
                 // Operación división:
                 respuesta = (double) numero1 / numero2;
                 break;
-            
+             case '^' :
+                // Operación potenciacion:
+                respuesta = Math.pow(numero1, numero2);
+                break;
+               case '%' :
+                // Operación modulo:
+                respuesta = (double) numero1 % numero2;
+                break;
             // TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO.
                 
             default:
@@ -46,7 +53,7 @@ public class Calculadora {
         
         // Lectura de la operación a realizar
         // TODO: DEBES ADICIONAR EL SÍMBOLO DE ^ Y % PARA CALCULAR LA POTENCIA Y EL MÓDULO
-        System.out.println("Ingrese la operación (+, -, *, /)");
+        System.out.println("Ingrese la operación (+, -, *, /,^,%)");
         char operacion = lector.next().charAt(0);
         
         double respuesta = calcular(numero1, numero2, operacion);
